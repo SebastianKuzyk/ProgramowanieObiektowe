e9e1b5e1ebe0aa1349522d5bcebc66d498101732
SebastianKuzyk <162573602+SebastianKuzyk@users.noreply.github.com>
2024-03-07 15:03:14 +0100

    Add files via upload

---
+++ lab01/zad1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String imie;
+        int wiek;
+
+        wiek = 21;
+        imie = "Sebastian";
+
+        System.out.println("Imie:" + imie);
+        System.out.println("Wiek:" + wiek);
+    }
+}
