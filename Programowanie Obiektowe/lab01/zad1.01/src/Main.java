0738a8db080ad18341d6085f91331748ea804a1d
SebastianKuzyk <162573602+SebastianKuzyk@users.noreply.github.com>
2024-03-07 14:39:14 +0100

    Add files via upload

---
+++ Programowanie Obiektowe/lab01/zad1.01/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String imie;
+        int wiek;
+
+        imie = "Sebastian";
+        wiek =  21;
+
+        System.out.println("Imie: "+imie);
+        System.out.println("Wiek: "+wiek);
+    }
+}
