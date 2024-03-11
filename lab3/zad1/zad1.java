5b37d581b7c6cb380675f5f921f54529b0708379
SebastianKuzyk <162573602+SebastianKuzyk@users.noreply.github.com>
2024-03-11 10:48:03 +0100

    Add files via upload

---
+++ lab3/zad1/zad1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class lab02 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe studentow");
+        int liczbastudentow = scanner.nextInt();
+
+        int i = 0;
+        float punkty = 0;
+        while (i < liczbastudentow) {
+                System.out.print("Podaj liczbę punktów studenta nr " + (i + 1) + ": ");
+        float liczbapunktow = scanner.nextFloat();
+        punkty += liczbapunktow;
+        i++;
+        }
+
+        float srednia = punkty / liczbastudentow;
+        System.out.println("\nŚrednia liczba punktów: " + srednia);
+    }
+}
